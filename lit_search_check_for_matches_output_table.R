2f104effcbe19f1f8abdfee5236bb98346c4dcc1
cschine <casey@schine.net>
2024-07-11 10:47:46 -0400

    separate literature search functions and code to run the functions

--- lit_search_check_for_matches_output_table.R
+++ lit_search_check_for_matches_output_table.R
@@ -1,5 +1,8 @@
 # script to read in literature search results and check for duplicates
-
+# this file contains all of the functions developed to read in the 
+# literature search results and remove duplicate records
+# the final function outputs a table with a single record containing
+# the search information for all duplicates
 
 library(stringr) #to trim file name strings
 library(tidyverse) #for everything
@@ -399,26 +402,3 @@ remove_duplicate_records <- function(match_tbl, record_tbl){
   return(result_list)
 }
 #################################################################################
-
-
-############# Run the Functions ##############
-# create file list for target directory 
-target_dir <- "./lit_search_results/test_more/"
-
-# create list of all search results in target directory
-#search_results_list <- read_search_results_from_directory(target_dir, output="list")
-
-# create tibble of all search results in target directory
-search_results_all_tbl <- read_search_results_from_directory(
-  target_dir, output="tibble", RecordID=TRUE, begin_record_num=2000)
-
-# create a tibble with target recordID and record IDs for potential matches
-result_match_tbl <- create_string_match_tbl_for_results(search_results_all_tbl,
-                                                        threshold=10,
-                                                        max_matches=24)
-
-write.csv(result_match_tbl, "./lit_search_results/moss_batch_initial_match_tbl.csv")
-
-confirmed_match_tbl <- user_comfirmation_of_matches(result_match_tbl, search_results_all_tbl)
-
-deduplicated_results_tbl <- remove_duplicate_records()
