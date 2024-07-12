1d55ab4c386cb78ed7830b48a8538ae84ec0efdc
robin11andresen <randresen@middlebury.edu>
2024-07-12 15:44:55 -0400

    add code chunks for sphagnum, pleurozium in run_lit_search file

--- run_lit_search_deduplication.R
+++ run_lit_search_deduplication.R
@@ -47,7 +47,7 @@ target_dir <- "./lit_search_results/bryophyte_batch/"
 
 # create tibble of all search results in target directory
 bryophyte_search_results_all_tbl <- read_search_results_from_directory(
-  target_dir, output="tibble", RecordID=TRUE, begin_record_num=1)
+  target_dir, output="tibble", RecordID=TRUE, begin_record_num=20000)
 
 # create a tibble with target recordID and record IDs for potential matches
 bryophyte_result_match_tbl <- create_string_match_tbl_for_results(bryophyte_search_results_all_tbl,
@@ -64,3 +64,65 @@ bryophyte_deduplicated_results_tbl <- remove_duplicate_records(bryophyte_confirm
 
 write.csv(bryophyte_deduplicated_results_tbl[[1]], "./lit_search_results/bryophyte_batch_deduplicated_result_tbl.csv")
 
+####### SPHAGNUM ########
+
+#### Read in the functions ####
+source("lit_search_check_for_matches_output_table.R")
+
+############# Run the Functions ##############
+# create file list for target directory 
+target_dir <- "./lit_search_results/sphagnum_batch/"
+
+# create list of all search results in target directory
+#search_results_list <- read_search_results_from_directory(target_dir, output="list")
+
+# create tibble of all search results in target directory
+sphagnum_search_results_all_tbl <- read_search_results_from_directory(
+  target_dir, output="tibble", RecordID=TRUE, begin_record_num=50000)
+
+# create a tibble with target recordID and record IDs for potential matches
+sphagnum_result_match_tbl <- create_string_match_tbl_for_results(sphagnum_search_results_all_tbl,
+                                                                  threshold=10,
+                                                                  max_matches=100)
+
+write.csv(sphagnum_result_match_tbl, "./lit_search_results/sphagnum_batch_initial_match_tbl.csv")
+
+sphagnum_confirmed_match_tbl <- user_comfirmation_of_matches(sphagnum_result_match_tbl, sphagnum_search_results_all_tbl)
+
+write.csv(sphagnum_confirmed_match_tbl, "./lit_search_results/sphagnum_batch_confirmed_match_tbl.csv")
+
+sphagnum_deduplicated_results_tbl <- remove_duplicate_records(sphagnum_confirmed_match_tbl,sphagnum_search_results_all_tbl)
+
+write.csv(sphagnum_deduplicated_results_tbl[[1]], "./lit_search_results/sphagnum_batch_deduplicated_result_tbl.csv")
+
+
+####### PLEUROZIUM ########
+
+#### Read in the functions ####
+source("lit_search_check_for_matches_output_table.R")
+
+############# Run the Functions ##############
+# create file list for target directory 
+target_dir <- "./lit_search_results/pleurozium_batch/"
+
+# create list of all search results in target directory
+#search_results_list <- read_search_results_from_directory(target_dir, output="list")
+
+# create tibble of all search results in target directory
+pleurozium_search_results_all_tbl <- read_search_results_from_directory(
+  target_dir, output="tibble", RecordID=TRUE, begin_record_num=70000)
+
+# create a tibble with target recordID and record IDs for potential matches
+pleurozium_result_match_tbl <- create_string_match_tbl_for_results(pleurozium_search_results_all_tbl,
+                                                                 threshold=10,
+                                                                 max_matches=100)
+
+write.csv(pleurozium_result_match_tbl, "./lit_search_results/pleurozium_batch_initial_match_tbl.csv")
+
+pleurozium_confirmed_match_tbl <- user_comfirmation_of_matches(pleurozium_result_match_tbl, pleurozium_search_results_all_tbl)
+
+write.csv(pleurozium_confirmed_match_tbl, "./lit_search_results/pleurozium_batch_confirmed_match_tbl.csv")
+
+pleurozium_deduplicated_results_tbl <- remove_duplicate_records(pleurozium_confirmed_match_tbl,pleurozium_search_results_all_tbl)
+
+write.csv(pleurozium_deduplicated_results_tbl[[1]], "./lit_search_results/pleurozium_batch_deduplicated_result_tbl.csv")
