a6909872c2b12fb326150fe0790c442557961d5c
robin11andresen <randresen@middlebury.edu>
2024-07-17 13:27:14 -0400

    done deduplicating liverwort_batch

--- run_lit_search_deduplication.R
+++ run_lit_search_deduplication.R
@@ -158,3 +158,33 @@ syntrichia_deduplicated_results_tbl <- remove_duplicate_records(syntrichia_confi
 
 write.csv(syntrichia_deduplicated_results_tbl[[1]], "./lit_search_results/syntrichia_batch_deduplicated_result_tbl.csv")
 
+####### LIVERWORT ########
+
+#### Read in the functions ####
+source("lit_search_check_for_matches_output_table.R")
+
+############# Run the Functions ##############
+# create file list for target directory 
+target_dir <- "./lit_search_results/liverwort_batch/"
+
+# create list of all search results in target directory
+#search_results_list <- read_search_results_from_directory(target_dir, output="list")
+
+# create tibble of all search results in target directory
+liverwort_search_results_all_tbl <- read_search_results_from_directory(
+  target_dir, output="tibble", RecordID=TRUE, begin_record_num=110000)
+
+# create a tibble with target recordID and record IDs for potential matches
+liverwort_result_match_tbl <- create_string_match_tbl_for_results(liverwort_search_results_all_tbl,
+                                                                   threshold=10,
+                                                                   max_matches=100)
+
+write.csv(liverwort_result_match_tbl, "./lit_search_results/liverwort_batch_initial_match_tbl.csv")
+
+liverwort_confirmed_match_tbl <- user_comfirmation_of_matches(liverwort_result_match_tbl, liverwort_search_results_all_tbl)
+
+write.csv(liverwort_confirmed_match_tbl, "./lit_search_results/liverwort_batch_confirmed_match_tbl.csv")
+
+liverwort_deduplicated_results_tbl <- remove_duplicate_records(liverwort_confirmed_match_tbl,liverwort_search_results_all_tbl)
+
+write.csv(liverwort_deduplicated_results_tbl[[1]], "./lit_search_results/liverwort_batch_deduplicated_result_tbl.csv")
